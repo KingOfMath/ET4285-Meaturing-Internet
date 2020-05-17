dbf66204f73e6d0ed3fce963beedc40ddf2f16c0
Kushal Prakash <f2014624@goa.bits-pilani.ac.in>
2020-05-17 16:02:23 +0200

    inference code with the mininet directory
    Signed-off-by: Kushal Prakash <f2014624@goa.bits-pilani.ac.in>

---
+++ mininet-master/inference.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+import gym
+from stable_baselines import PPO1
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.net import Mininet
+from mininet.cli import CLI
+
+
+def main():
+
+    # Load a .zip model file
+    model_name = os.path.join(os.getcwd(), "ppo1_cc_0.zip")
+    model = PPO1.load(model_name)
+
+    # TODO start the mininet network
+
+    class SingleSwitchTopo( Topo ):                                                                                               
+                                                                                                      
+      def build( self, count=1 ):                                                                                      
+        hosts = [ self.addHost( 'h%d' % i ,1.0)                                                                                   
+                  for i in range( 1, count + 1 ) ]                                                                                
+        s1 = self.addSwitch( 's1' )                                                                                           
+        for h in hosts:                                                                                                       
+            self.addLink( h, s1, bw=10, delay='5ms', loss=2,
+                          max_queue_size=1000, use_htb=True )                                                                                             
+    # link rate = 10, max queue zie= 1000, loss percentage= 2%
+    net = Mininet( topo=SingleSwitchTopo( 3 ) )                                                                               
+    net.start()                                                                                                               
+    dumpNodeConnections(net.hosts)
+    
+    h1,h4=net.get('h1','h3')
+    net.pingFull([h1,h4])
+    net.monitor()
+    perf_array=net.iperf((h1,h4))                                                                                                                
+    net.stop()   
+    latency_grad=0
+    print(perf_array[0])
+
+    # this model takes as state an input 30x1
+    # TODO: this needs to be replaced with real values from the network later on
+    state = np.ones((30,), dtype=np.float32)
+
+    # TODO start traffic
+
+    # run inference for some steps
+    count = 0
+    # while (time.time()-start) < 3:
+    while count < 10:
+
+        # The action is an increment to the current sending rate: sending_rate_t+1 = sending_rate_t + action
+        action, _ = model.predict(state)
+        print("RL model predicted: %s" % action)
+
+        # Update the state to account for effects of action
+        # TODO: this needs to be replaced with real values from the network later on
+        state = state * 1.1
+        count += 1
+
+if __name__ == '__main__':
+    main()
