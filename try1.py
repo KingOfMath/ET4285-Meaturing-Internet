d1951e8dfafb351c3c7e4e647c7850cf594b3f6b
KingOfMath <823221784@qq.com>
2020-06-03 15:56:46 +0200

    Add files via upload

--- try1.py
+++ try1.py
@@ -1,35 +1,40 @@
-def main():
-    import numpy as np
-    data_1 = loadData('C:/Users/18969/Desktop/Measuring/Data/Aurora_Sender.dat')
-    length_1 = len(data_1)
-    
-    data_2 = loadData('C:/Users/18969/Desktop/Measuring/Data/Aurora_Sender_test.txt')
-    length_2 = len(data_2)    
+import sys, getopt
 
-    SendRate_1 = average(extractSendRate(data_1, length_1))
-    SendRate_2 = average(extractSendRate(data_2, length_2))
-    
-    RTT_1 = average(extractRTT(data_1, length_1))
-    RTT_2 = average(extractRTT(data_2, length_2))
-    
+
+def run(argv):
+    _, args = getopt.getopt(argv, "null")
+    file = open('data/fairness.txt', 'w')
+
+    data_1 = loadData(args[0])
+    length_1 = len(data_1)
+
+    data_2 = loadData(args[1])
+    length_2 = len(data_2)
+
+    SendRate_1 = extractSendRate(data_1, length_1)
+    SendRate_2 = extractSendRate(data_2, length_2)
+    for i in range(int(length_1 / 4)):
+        SR_1 = SendRate_1[i]
+        SR_2 = SendRate_2[i]
+        f = fairness(SR_1, SR_2)
+        file.write(str(f) + "\n")
+    file.close()
+
+    RTT_1 = extractRTT(data_1, length_1)
+    RTT_2 = extractRTT(data_2, length_2)
+    print(RTT_1)
+    print(RTT_2)
+
     Loss_1 = average(extractLoss(data_1, length_1))
     Loss_2 = average(extractLoss(data_2, length_2))
 
-    
     ACK_1 = average(extractACK(data_1, length_1))
     ACK_2 = average(extractACK(data_2, length_2))
 
     if 1:
-        print(SendRate_1)
-        print(SendRate_2)
-        SendRateFairness = fairness(SendRate_1, SendRate_2)
+        SendRateFairness = fairness(average(SendRate_1), average(SendRate_2))
         print(SendRateFairness)
 
-    if 0:
-        print(RTT_1)
-        print(RTT_2)
-        RTTFairness = fairness(RTT_1, RTT_2)
-        print(RTTFairness)
 
     if 0:
         print(Loss_1)
@@ -41,48 +46,55 @@ def main():
         print(ACK_1)
         print(ACK_2)
         ACKFairness = fairness(ACK_1, ACK_2)
-        print(ACKFairness)            
+        print(ACKFairness)
 
-    
+
 def average(data):
-    return sum(data)/len(data)
-    
+    return sum(data) / len(data)
+
+
 def fairness(x1, x2):
-    return pow(x1+x2,2)/(2*(pow(x1,2)+pow(x2,2)))
-    
+    return pow(x1 + x2, 2) / (2 * (pow(x1, 2) + pow(x2, 2)))
+
+
 def extractSendRate(data, length):
     SendRate = []
     i = 0
-    for i in range(0,length-1):
-        if (i+1)%4 == 1:
+    for i in range(0, length - 1):
+        if (i + 1) % 4 == 1:
             SendRate.append(float(data[i]))
     return SendRate
 
+
 def extractRTT(data, length):
     RTT = []
-    for i in range(0,length-1):
-        if (i+1)%4 == 2:
+    for i in range(0, length - 1):
+        if (i + 1) % 4 == 2:
             RTT.append(float(data[i]))
     return RTT
 
+
 def extractLoss(data, length):
     Loss = []
-    for i in range(0,length-1):
-        if (i+1)%4 == 3:
+    for i in range(0, length - 1):
+        if (i + 1) % 4 == 3:
             Loss.append(float(data[i]))
     return Loss
 
+
 def extractACK(data, length):
     ACK = []
-    for i in range(0,length-1):
-        if (i+1)%4 == 0:
+    for i in range(0, length - 1):
+        if (i + 1) % 4 == 0:
             ACK.append(float(data[i]))
     return ACK
+
 
 def loadData(filename):
     with open(filename) as proto1:
         temp = proto1.read().splitlines()
         return temp
 
-    
-main()
+
+if __name__ == '__main__':
+    run(sys.argv[1:])
