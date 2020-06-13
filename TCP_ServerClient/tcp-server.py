f9ec8e3ae369611c01c7d073f010da2b5e9781dc
Kushal Prakash <f2014624@goa.bits-pilani.ac.in>
2020-06-13 03:51:29 +0200

    added pcc files
    Signed-off-by: Kushal Prakash <f2014624@goa.bits-pilani.ac.in>

---
+++ TCP_ServerClient/tcp-server.py
@@ -0,0 +1,12 @@
+import os
+
+sys.system('iperf3 ')
+#network  
+#what is specific about the problem?
+#what users don't like!
+#cehck for session based recommendation
+#session based recommendation
+#what relevant information 
+#tourist destination reommenation
+#session based recommendation
+# 
